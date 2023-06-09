1d84784a8785c3bd76ab1de00641d3180dbfeaed
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-06-09 02:02:22 -0300

    Criado layout dos dados do veiculo
    Criado layout dos dados do veiculo

--- app/src/main/java/com/example/custofrete/presentation/entrega/EntregaFragment.kt
+++ app/src/main/java/com/example/custofrete/presentation/entrega/EntregaFragment.kt
@@ -1,11 +1,14 @@
 package com.example.custofrete.presentation.entrega
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.custofrete.R
 import com.example.custofrete.databinding.FragmentEntregaBinding
 
 class EntregaFragment : Fragment() {
@@ -20,12 +23,51 @@ class EntregaFragment : Fragment() {
         _binding = FragmentEntregaBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.btDadosVeiculo.setOnClickListener {
+            dadosVeiculo(requireContext())
+        }
+
         binding.btCalcular.setOnClickListener {
             val action = EntregaFragmentDirections.actionEntregaFragmentToCalculoFragment()
             findNavController().navigate(action)
         }
 
         return root
+    }
+    private fun dadosVeiculo(contextTela : Context){
+
+        val builder = AlertDialog.Builder(contextTela!!)
+
+        val view: View
+        val inflater = contextTela!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        view = inflater.inflate(R.layout.dialogo_dados_veiculo,null)
+
+       // val spState = view.findViewById<Spinner>(R.id.spStateSearch)
+
+
+        builder.setView(view)
+
+        builder.setPositiveButton("OK") { dialog, which -> }
+
+        builder.setNegativeButton("Cancel", null)
+
+
+        val dialog = builder.create()
+
+        dialog.setOnShowListener {
+            val button = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            button.setOnClickListener {
+
+
+//                if(position <= 0 ){
+//                    val text = "Selecione uma cidade"
+//                    tvCity.setError(text)
+//                }else{
+//                    dialog.dismiss()
+//                }
+            }
+        }
+        dialog.show()
     }
 
 }
