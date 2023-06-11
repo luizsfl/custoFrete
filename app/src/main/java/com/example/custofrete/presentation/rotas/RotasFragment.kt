52d971d62cc61b84c1aa767574b8cb942d2988f7
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-06-11 19:54:11 -0300

    Finalizado ajuste da busca dos endereços direto no google

--- app/src/main/java/com/example/custofrete/presentation/rotas/RotasFragment.kt
+++ app/src/main/java/com/example/custofrete/presentation/rotas/RotasFragment.kt
@@ -1,24 +1,24 @@
 package com.example.custofrete.presentation.rotas
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.custofrete.R
 import com.example.custofrete.databinding.FragmentRotasBinding
 import com.example.custofrete.presentation.adapter.PlaceAutoSuggestAdapter
+import com.google.android.gms.maps.model.LatLng
 
 
 class RotasFragment : Fragment() {
 
     private var _binding: FragmentRotasBinding? = null
     private val binding get() = _binding!!
-
-    val arrayOfStrings = arrayOf("alfhanistan","albania","alegri","andorra","angola")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,20 +27,12 @@ class RotasFragment : Fragment() {
         _binding = FragmentRotasBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val arrayAdapter = ArrayAdapter(requireContext(),
-            R.layout.autocomplete_list_rotas,
-            R.id.tv_title_rota_list,
-            arrayOfStrings.toList()
-        )
-
         binding.btEnderecoEntrega.setAdapter(
             PlaceAutoSuggestAdapter(
                 binding.root.context,
                 android.R.layout.simple_list_item_1
             )
         )
-
-        //binding.btEnderecoEntrega.setAdapter(arrayAdapter)
 
         (activity as AppCompatActivity).supportActionBar?.hide()
 
@@ -49,6 +41,66 @@ class RotasFragment : Fragment() {
             findNavController().navigate(action)
         }
 
+        selectRota()
+
         return root
+
     }
+
+    private fun selectRota() {
+        binding.btEnderecoEntrega.setOnItemClickListener { parent, view, position, id ->
+            Log.d("Address : ", binding.btEnderecoEntrega.getText().toString())
+            val latLng: LatLng? =
+                getLatLngFromAddress(binding.btEnderecoEntrega.getText().toString())
+            if (latLng != null) {
+                Log.d("Lat Lng : ", " " + latLng.latitude + " " + latLng.longitude)
+                val address: Address? = getAddressFromLatLng(latLng)
+                if (address != null) {
+                    Log.d("tagtest_Address : ", "" + address.toString())
+                    Log.d("tagtest_Address Line : ", "" + address.getAddressLine(0))
+                    Log.d("tagtest_Phone : ", "" + address.getPhone())
+                    Log.d("tagtest_Pin Code : ", "" + address.getPostalCode())
+                    Log.d("tagtest_Feature : ", "" + address.getFeatureName())
+                    Log.d("tagtest_More : ", "" + address.getLocality())
+                } else {
+                    Log.d("Adddress", "Address Not Found")
+                }
+            } else {
+                Log.d("Lat Lng", "Lat Lng Not Found")
+            }
+        }
+    }
+
+    private fun getLatLngFromAddress(address: String): LatLng? {
+        val geocoder = Geocoder(requireContext())
+        val addressList: List<Address>?
+        return try {
+            addressList = geocoder.getFromLocationName(address, 1)
+            if (addressList != null) {
+                val singleaddress = addressList[0]
+
+                return LatLng(singleaddress.latitude, singleaddress.longitude)
+
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    private fun getAddressFromLatLng(latLng: LatLng): Address? {
+        val geocoder = Geocoder(requireContext())
+        val addresses: List<Address>?
+        return try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 5)
+            addresses?.get(0)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+
 }
