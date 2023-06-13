e87a4f1f591ee5e7507e3e02ca4fe7b8ee7f2058
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-06-13 00:40:44 -0300

    Fonte para implementar a rota entre dois pontos

--- app/src/main/java/com/example/custofrete/presentation/rotas/RotasFragment.kt
+++ app/src/main/java/com/example/custofrete/presentation/rotas/RotasFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.*
 import android.graphics.drawable.VectorDrawable
 import android.location.Address
 import android.location.Geocoder
+import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -16,15 +17,17 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.custofrete.R
 import com.example.custofrete.databinding.FragmentRotasBinding
+import com.example.custofrete.domain.model.GoogleMapDTO
+import com.example.custofrete.domain.model.Location
 import com.example.custofrete.domain.model.Rota
 import com.example.custofrete.presentation.adapter.PlaceAutoSuggestAdapter
 import com.example.custofrete.presentation.adapter.RotaAdapter
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.squareup.okhttp.OkHttpClient
+import com.google.gson.Gson
+import com.squareup.okhttp.Request
 
 
 class RotasFragment : Fragment() {
@@ -32,6 +35,7 @@ class RotasFragment : Fragment() {
     private var _binding: FragmentRotasBinding? = null
     private val binding get() = _binding!!
     private val listaRota: MutableList<Rota> = mutableListOf()
+    lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -89,15 +93,29 @@ class RotasFragment : Fragment() {
                         latLng = latLng
                     )
 
-                    binding.llMapa.getMapAsync { google ->
-                        google.moveCamera(CameraUpdateFactory.newLatLngZoom(rota.latLng, 13f))
-                        addMarkers(google)
-                    }
-
                     listaRota.add(rota)
                     val listaRotaInvest = listaRota.map { it.copy() }
                     setHomeListAdapter(listaRotaInvest.reversed())
                     binding.btEnderecoEntrega.setText("")
+
+                    binding.llMapa.getMapAsync { google ->
+                        val location1 = rota.latLng
+                        google.moveCamera(CameraUpdateFactory.newLatLngZoom(location1, 13f))
+                        addMarkers(google)
+
+                        if (listaRota.size>1){
+                            googleMap = google
+
+                            val location2 = listaRota.get(listaRota.size-2).latLng
+
+                            // Log.d("GoogleMap", "before URL")
+                            var URL = getDirectionURL(location1,location2)
+                            // Log.d("GoogleMap", "URL : $URL")
+                            GetDirection(URL).execute()
+
+                        }
+
+                    }
 
                 } else {
                     Log.d("Adddress", "Address Not Found")
@@ -190,4 +208,86 @@ class RotasFragment : Fragment() {
         canvas.drawText(id.toString(), bitmap.width / 2f, bitmap.height / 2f + 10, paint)
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
+
+    fun getDirectionURL(origin:LatLng,dest:LatLng) : String{
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving&key=AIzaSyA2TWLwHJhNZtJ867ipr_5XhQZMGKm49Os"
+    }
+
+    private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body()!!.string()
+            Log.d("GoogleMap" , " data : $data")
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data, GoogleMapDTO::class.java)
+
+                val path =  ArrayList<LatLng>()
+
+                for (i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
+//                    path.add(startLatLng)
+//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
+//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+            googleMap.addPolyline(lineoption)
+        }
+    }
+
+    public fun decodePolyline(encoded: String): List<LatLng> {
+
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
+    }
+
 }
