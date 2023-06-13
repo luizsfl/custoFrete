c1d92b3c6326cf757001dcce6c5dcbdf3c4567bb
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-06-13 13:21:59 -0300

    Inicio do problema do caixeiro viajante para pegar a menor rota

--- app/src/main/java/com/example/custofrete/presentation/calculo/CalculoFragment.kt
+++ app/src/main/java/com/example/custofrete/presentation/calculo/CalculoFragment.kt
@@ -20,9 +20,56 @@ class CalculoFragment : Fragment() {
         _binding = FragmentCalculoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val graph = arrayOf(
+           intArrayOf(0, 10, 15, 20), //10  1
+           intArrayOf(10, 0, 35, 25), // 25 0
+           intArrayOf(15, 35, 0, 30), //    3
+           intArrayOf(1, 25, 30, 0)  //    2
+        )
+
+        getMelhorRota(graph)
+
         (activity as AppCompatActivity).supportActionBar?.hide()
 
         return root
 
+    }
+
+    fun getMelhorRota(graph :Array<IntArray>) {
+        val tsp = TSP(graph)
+        println(tsp.getTour())
+    }
+
+    class TSP(private val graph: Array<IntArray>) {
+        private val n = graph.size
+        private val visited = BooleanArray(n)
+        private var minTourCost  = Int.MAX_VALUE
+        private var bestTourOrder: List<Int>? = null
+
+        fun getTour(): String {
+            tsp(0, mutableListOf(), 0)
+            return "A ordem ótima da turnê é $bestTourOrder e o custo mínimo é $minTourCost"
+        }
+
+
+
+        private fun tsp(currentVertex: Int, tourOrder: MutableList<Int>, currentCost: Int) {
+            if (tourOrder.size == n && graph[currentVertex][0] != 0) {
+                if (currentCost + graph[currentVertex][0] < minTourCost) {
+                    bestTourOrder = tourOrder.toList()
+                    minTourCost = currentCost + graph[currentVertex][0]
+                }
+                return
+            }
+            for (i in 0 until n) {
+                if (!visited[i] && graph[currentVertex][i] != 0) {
+                    visited[i] = true
+                    tourOrder.add(i)
+                    tsp(i, tourOrder, currentCost + graph[currentVertex][i])
+                    visited[i] = false
+                    tourOrder.removeAt(tourOrder.size - 1)
+                }
+            }
+        }
     }
 }
