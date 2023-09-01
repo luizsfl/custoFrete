e5761344c48ba1af8e96bc72b5b11cba4066c013
Pedro Luiz <70184804+luizsfl@users.noreply.github.com>
2023-09-01 00:07:31 -0300

    Opção para enviar mensagem

--- app/src/main/java/com/example/custofrete/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/custofrete/presentation/login/LoginFragment.kt
@@ -1,13 +1,19 @@
 package com.example.custofrete.presentation.login
 
 import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.InputType
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -52,6 +58,10 @@ class LoginFragment : Fragment() {
                 is ViewStateUsuario.Failure -> showErro(viewState.messengerError)
                 else -> {}
             }
+        }
+
+        binding.tvPrecisaAjuda.setOnClickListener {
+            showAlertDialogSendMensagem(requireContext())
         }
 
         binding.btEntrar.setOnClickListener {
@@ -121,5 +131,66 @@ class LoginFragment : Fragment() {
 
         showLoading(false)
     }
+
+    private fun showAlertDialogSendMensagem(contextTela:Context) {
+
+        val builder = AlertDialog.Builder(contextTela)
+        builder.setTitle("Envie uma mensagem, dúvida ou melhoria")
+
+        val input = EditText(contextTela)
+        input.setHint("Digite aqui")
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { dialog, which -> }
+
+        builder.setNegativeButton("Cancel", null)
+
+        val dialog = builder.create()
+
+        dialog.setOnShowListener {
+            val button = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            button.setOnClickListener {
+
+                var message = input.text.toString()
+
+                if(message.isEmpty()){
+                    val erro = "Digite a sua mensagem, dúvida ou melhoria. Campo ficou em branco"
+                    input.error = erro
+                }else{
+                    val destinatario = "luizsfl+custofrete@hotmail.com"
+                    val assunto = "Mensagem,duvida ou melhoria"
+
+                    sendEmail(requireContext(),destinatario,assunto, message)
+                    dialog.dismiss()
+
+                }
+            }
+        }
+
+        dialog.show()
+    }
+
+    private  fun  sendEmail(contextTela: Context, recipient: String, subject: String, message: String) {
+        val mIntent = Intent(Intent.ACTION_SEND)
+
+        mIntent.setPackage("com.google.android.gm");
+
+        mIntent.data = Uri.parse( "mailto:" )
+        mIntent.type = "text/plain"
+
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+        try {
+            startActivity(Intent.createChooser(mIntent, "Choose Email Client..." ))
+        }
+        catch (e: Exception){
+            Toast.makeText( contextTela , e.message, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
 
 }
